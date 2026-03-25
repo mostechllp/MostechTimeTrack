fc2b0c04b6f498bbf61d33ca7b41c271dc2a2d43
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-03-25 13:01:42 +0530

    updated allowed Origins in CORS

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://mostech-time-track-two.vercel.app',
+  'https://mostech.ae/time-tracker/',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
